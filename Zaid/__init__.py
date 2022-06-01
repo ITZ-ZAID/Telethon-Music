0772222af24c5ad3ef4f9cc47b1880123c70cc2b
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-06-01 11:16:59 +0530

    Update __init__.py

--- Zaid/__init__.py
+++ Zaid/__init__.py
@@ -11,5 +11,5 @@ API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 
-bot = TelegramClient('Zaid', api_id=Config.APP_ID, api_hash=Config.API_HASH)
+bot = TelegramClient('Zaid', api_id=APP_ID, api_hash=API_HASH)
 Zaid = bot.start(bot_token=BOT_TOKEN)
