4cac714375207ab4483067619666972fc3f52b10
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-06-01 17:44:35 +0530

    Update __init__.py

--- Zaid/__init__.py
+++ Zaid/__init__.py
@@ -11,14 +11,12 @@ from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.INFO)
 
-STRING_SESSION = os.environ.get("STRING_SESSION", None)
-API_ID = os.environ.get("API_ID", None)
-API_HASH = os.environ.get("API_HASH", None)
-BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 
-bot = TelegramClient('Zaid', api_id=API_ID, api_hash=API_HASH)
-Zaid = bot.start(bot_token=BOT_TOKEN)
-client = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH)
+from Config import Config
+
+bot = TelegramClient('Zaid', api_id=Config.API_ID, api_hash=Config.API_HASH)
+Zaid = bot.start(bot_token=Config.BOT_TOKEN)
+client = TelegramClient(StringSession(Config.STRING_SESSION), Config.API_ID, Config.API_HASH)
 call_py = PyTgCalls(client)
 client.start()
 call_py.start()
