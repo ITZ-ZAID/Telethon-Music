3151a0afee25eeefea57063e8638763d21a7fa81
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-06-01 11:13:45 +0530

    Create utils.py

---
+++ Zaid/utils.py
@@ -0,0 +1,14 @@
+import sys
+import logging
+import importlib
+from pathlib import Path
+
+def load_plugins(plugin_name):
+    path = Path(f"Zaid/plugins/{plugin_name}.py")
+    name = "GPBot.plugins.{}".format(plugin_name)
+    spec = importlib.util.spec_from_file_location(name, path)
+    load = importlib.util.module_from_spec(spec)
+    load.logger = logging.getLogger(plugin_name)
+    spec.loader.exec_module(load)
+    sys.modules["GPBot.plugins." + plugin_name] = load
+    print("Bot has Started " + plugin_name)
