500420e86f18e57b5b4838805904875f3fa6aff5
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-06-01 11:05:57 +0530

    Update __init__.py

--- Zaid/__init__.py
+++ Zaid/__init__.py
@@ -19,6 +19,7 @@ import os
 STRING_SESSION = os.environ.get("STRING_SESSION", None)
 API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
+BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 
 
 telethon = TelegramClient(
