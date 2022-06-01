29742334ca95b3b6bcbc34d3a3b07aa4e4f45559
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-06-01 11:15:01 +0530

    Update __init__.py

--- Zaid/__init__.py
+++ Zaid/__init__.py
@@ -2,10 +2,14 @@
 
 from telethon import TelegramClient
 import logging
-from Configs import Config
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.INFO)
 
+STRING_SESSION = os.environ.get("STRING_SESSION", None)
+API_ID = os.environ.get("API_ID", None)
+API_HASH = os.environ.get("API_HASH", None)
+BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+
 bot = TelegramClient('Zaid', api_id=Config.APP_ID, api_hash=Config.API_HASH)
-Zaid = bot.start(bot_token=Config.TOKEN)
+Zaid = bot.start(bot_token=BOT_TOKEN)
