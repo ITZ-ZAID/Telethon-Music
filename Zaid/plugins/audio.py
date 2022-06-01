55b35cc30d6a5d5fac11edcf1e07c870dc1ba267
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-06-01 14:37:22 +0530

    Update audio.py

--- Zaid/plugins/audio.py
+++ Zaid/plugins/audio.py
@@ -147,7 +147,7 @@ async def play(event):
             ) 
     if event.is_group:
         try:
-            await Client(ImportChatInviteRequest(privates))
+            await Client(functions.channels.JoinChannelRequest(channel=public))
         except Exception as e:
             print(e)
             pass    
