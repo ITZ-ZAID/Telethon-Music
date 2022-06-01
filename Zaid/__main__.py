d94d3ccb67a3259f63c0ab1d23eb505f2512bbae
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-06-01 11:11:25 +0530

    Update __main__.py

--- Zaid/__main__.py
+++ Zaid/__main__.py
@@ -1,5 +1,25 @@
-import sys
-from pytgcalls import idle
-from Zaid import telethon, call_py
+
+
+
+import glob
+from pathlib import Path
+from GPBot.utils import load_plugins
+import logging
+from GPBot import Stark
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.INFO)
+
 path = "Zaid/plugins/*.py"
-telethon.run_until_disconnected()
+files = glob.glob(path)
+for name in files:
+    with open(name) as a:
+        patt = Path(a.name)
+        plugin_name = patt.stem
+        load_plugins(plugin_name.replace(".py", ""))
+    
+print("Successfully Started Bot!")
+print("Visit @TgxBotz")
+
+if __name__ == "__main__":
+    Stark.run_until_disconnected()
