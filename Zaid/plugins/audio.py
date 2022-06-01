874cbacf159c7e8890648fd9d0cce29068e559b3
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-06-01 11:53:48 +0530

    Create audio.py

---
+++ Zaid/plugins/audio.py
@@ -0,0 +1,299 @@
+# Credits: @mrismanaziz
+# Thanks To @tofik_dn || https://github.com/tofikdn
+# FROM Man-Userbot <https://github.com/mrismanaziz/Man-Userbot>
+# t.me/SharingUserbot & t.me/Lunatic0de
+
+from pytgcalls import StreamType
+from pytgcalls.types import Update
+from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
+from pytgcalls.types.input_stream.quality import (
+    HighQualityAudio,
+    HighQualityVideo,
+    LowQualityVideo,
+    MediumQualityVideo,
+)
+from pytgcalls.exceptions import (
+    NoActiveGroupCall,
+    NotInGroupCallError
+)
+from telethon.tl import types
+from telethon.utils import get_display_name
+from telethon.tl.functions.users import GetFullUserRequest
+from youtubesearchpython import VideosSearch
+
+ 
+fotoplay = "https://telegra.ph/file/fbdff93b72dac4e43a9af.png"
+ngantri = "https://telegra.ph/file/fbdff93b72dac4e43a9af.png"
+from Zaid import call_py, Zaid 
+owner = "1669178360"
+from helpers.chattitle import CHAT_TITLE
+from helpers.queues import (
+    QUEUE,
+    add_to_queue,
+    clear_queue,
+    get_queue,
+    pop_an_item,
+)
+from helpers.thumbnail import gen_thumb
+
+
+def vcmention(user):
+    full_name = get_display_name(user)
+    if not isinstance(user, types.User):
+        return full_name
+    return f"[{full_name}](tg://user?id={user.id})"
+
+
+def ytsearch(query: str):
+    try:
+        search = VideosSearch(query, limit=1).result()
+        data = search["result"][0]
+        songname = data["title"]
+        url = data["link"]
+        duration = data["duration"]
+        thumbnail = f"https://i.ytimg.com/vi/{data['id']}/hqdefault.jpg"
+        return [songname, url, duration, thumbnail]
+    except Exception as e:
+        print(e)
+        return 0
+
+
+async def ytdl(format: str, link: str):
+    stdout, stderr = await bash(f'yt-dlp -g -f "{format}" {link}')
+    if stdout:
+        return 1, stdout.split("\n")[0]
+    return 0, stderr
+
+
+async def skip_item(chat_id: int, x: int):
+    if chat_id not in QUEUE:
+        return 0
+    chat_queue = get_queue(chat_id)
+    try:
+        songname = chat_queue[x][0]
+        chat_queue.pop(x)
+        return songname
+    except Exception as e:
+        print(e)
+        return 0
+
+
+async def skip_current_song(chat_id: int):
+    if chat_id not in QUEUE:
+        return 0
+    chat_queue = get_queue(chat_id)
+    if len(chat_queue) == 1:
+        await call_py.leave_group_call(chat_id)
+        clear_queue(chat_id)
+        return 1
+    songname = chat_queue[1][0]
+    url = chat_queue[1][1]
+    link = chat_queue[1][2]
+    type = chat_queue[1][3]
+    RESOLUSI = chat_queue[1][4]
+    if type == "Audio":
+        await call_py.change_stream(
+            chat_id,
+            AudioPiped(
+                url,
+            ),
+        )
+    elif type == "Video":
+        if RESOLUSI == 720:
+            hm = HighQualityVideo()
+        elif RESOLUSI == 480:
+            hm = MediumQualityVideo()
+        elif RESOLUSI == 360:
+            hm = LowQualityVideo()
+        await call_py.change_stream(
+            chat_id, AudioVideoPiped(url, HighQualityAudio(), hm)
+        )
+    pop_an_item(chat_id)
+    return [songname, link, type]
+
+
+#play
+async def vc_play(event):
+    title = event.pattern_match.group(1)
+    replied = await event.get_reply_message()
+    sender = await event.get_sender()
+    chat = await event.get_chat()
+    chat_id = event.chat_id
+    from_user = vcmention(event.sender)
+    if (
+        replied
+        and not replied.audio
+        and not replied.voice
+        and not title
+        or not replied
+        and not title
+    ):
+        return await event.reply("**Processing**")
+    elif replied and not replied.audio and not replied.voice or not replied:
+        botman = await event.reply("`Featching Details...`")
+        query = event.text.split(maxsplit=1)[1]
+        search = ytsearch(query)
+        if search == 0:
+            await botman.edit(
+                "**Can't Find Song** Try searching with More Specific Title"
+            )
+        else:
+            songname = search[0]
+            title = search[0]
+            url = search[1]
+            duration = search[2]
+            thumbnail = search[3]
+            userid = sender.id
+            titlegc = chat.title
+            ctitle = await CHAT_TITLE(titlegc)
+            thumb = await gen_thumb(thumbnail, title, userid, ctitle)
+            format = "best[height<=?720][width<=?1280]"
+            hm, ytlink = await ytdl(format, url)
+            if hm == 0:
+                await botman.edit(f"`{ytlink}`")
+            elif chat_id in QUEUE:
+                pos = add_to_queue(chat_id, songname, ytlink, url, "Audio", 0)
+                caption = f"💡 **Song Added To queue »** `#{pos}`\n\n**🏷 Name:** [{songname}]({url})\n**⏱ Duration:** `{duration}`\n🎧 **Requester:** {from_user}"
+                await botman.delete()
+                await event.client.send_file(chat_id, thumb, caption=caption)
+            else:
+                try:
+                    await call_py.join_group_call(
+                        chat_id,
+                        AudioPiped(
+                            ytlink,
+                        ),
+                        stream_type=StreamType().pulse_stream,
+                    )
+                    add_to_queue(chat_id, songname, ytlink, url, "Audio", 0)
+                    caption = f"🏷 **Name:** [{songname}]({url})\n**⏱ Duration:** `{duration}`\n💡 **Status:** `Playing`\n🎧 **Requester:** {from_user}"
+                    await botman.delete()
+                    await event.client.send_file(chat_id, thumb, caption=caption)
+                except Exception as ep:
+                    clear_queue(chat_id)
+                    await botman.edit(f"`{ep}`")
+
+    else:
+        botman = await edit_or_reply(event, "📥 **Downloading**")
+        dl = await replied.download_media()
+        link = f"https://t.me/c/{chat.id}/{event.reply_to_msg_id}"
+        if replied.audio:
+            songname = "Telegram Music Player"
+        elif replied.voice:
+            songname = "Voice Note"
+        if chat_id in QUEUE:
+            pos = add_to_queue(chat_id, songname, dl, link, "Audio", 0)
+            caption = f"💡 **Song Added To queue »** `#{pos}`\n\n**🏷 Title:** [{songname}]({link})\n**👥 Chat ID:** `{chat_id}`\n🎧 **Requester:** {from_user}"
+            await event.client.send_file(chat_id, ngantri, caption=caption)
+            await botman.delete()
+        else:
+            try:
+                await call_py.join_group_call(
+                    chat_id,
+                    AudioPiped(
+                        dl,
+                    ),
+                    stream_type=StreamType().pulse_stream,
+                )
+                add_to_queue(chat_id, songname, dl, link, "Audio", 0)
+                caption = f"🏷 **Title:** [{songname}]({link})\n**👥 Chat ID:** `{chat_id}`\n💡 **Status:** `Playing`\n🎧 **Requested:** {from_user}"
+                await event.client.send_file(chat_id, fotoplay, caption=caption)
+                await botman.delete()
+            except Exception as ep:
+                clear_queue(chat_id)
+                await botman.edit(f"`{ep}`")
+
+
+
+
+
+#end
+async def vc_end(event):
+    chat_id = event.chat_id
+    if chat_id in QUEUE:
+        try:
+            await call_py.leave_group_call(chat_id)
+            clear_queue(chat_id)
+            await event.reply("**Ntg Streaming**")
+        except Exception as e:
+            await event.reply(f"**ERROR:** `{e}`")
+    else:
+        await event.reply("**Ntg is Streaming**")
+
+
+
+
+
+
+
+
+
+
+#playlist
+async def vc_playlist(event):
+    chat_id = event.chat_id
+    if chat_id in QUEUE:
+        chat_queue = get_queue(chat_id)
+        if len(chat_queue) == 1:
+            await event.reply(
+                f"**�PlAYLIST:**\n• [{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}`",
+                link_preview=False,
+            )
+        else:
+            PLAYLIST = f"**🎧 PLAYLIST:**\n**• [{chat_queue[0][0]}]({chat_queue[0][2]})** | `{chat_queue[0][3]}` \n\n**• Daftar Putar:**"
+            l = len(chat_queue)
+            for x in range(1, l):
+                hmm = chat_queue[x][0]
+                hmmm = chat_queue[x][2]
+                hmmmm = chat_queue[x][3]
+                PLAYLIST = PLAYLIST + "\n" + \
+                    f"**#{x}** - [{hmm}]({hmmm}) | `{hmmmm}`"
+            await event.reply(PLAYLIST, link_preview=False)
+    else:
+        await event.reply("**Ntg is Streaming**")
+
+
+
+
+
+
+#leavevc
+async def leavevc(event):
+    """ leave video chat """
+    xnxx = await event.reply("Processing")
+    chat_id = event.chat_id
+    from_user = vcmention(event.sender)
+    if from_user:
+        try:
+            await call_py.leave_group_call(chat_id)
+        except (NotInGroupCallError, NoActiveGroupCall):
+            pass
+        await xnxx.edit("**Left the voice chat** `{}`".format(str(event.chat_id)))
+    else:
+        await xnxx.edit(f"**Sorry {owner} not on Voice Chat**")
+
+
+@call_py.on_stream_end()
+async def stream_end_handler(_, u: Update):
+    chat_id = u.chat_id
+    print(chat_id)
+    await skip_current_song(chat_id)
+
+
+@call_py.on_closed_voice_chat()
+async def closedvc(_, chat_id: int):
+    if chat_id in QUEUE:
+        clear_queue(chat_id)
+
+
+@call_py.on_left()
+async def leftvc(_, chat_id: int):
+    if chat_id in QUEUE:
+        clear_queue(chat_id)
+
+
+@call_py.on_kicked()
+async def kickedvc(_, chat_id: int):
+    if chat_id in QUEUE:
+        clear_queue(chat_id)
+
