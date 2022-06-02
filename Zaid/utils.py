5a7c5cd44ba0768d6132a8708233bb15f74d7d35
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-06-02 12:55:28 +0530

    Update utils.py

--- Zaid/utils.py
+++ Zaid/utils.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 def load_plugins(plugin_name):
     path = Path(f"Zaid/plugins/{plugin_name}.py")
-    name = "GPBot.plugins.{}".format(plugin_name)
+    name = "Zaid.plugins.{}".format(plugin_name)
     spec = importlib.util.spec_from_file_location(name, path)
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugin_name)
