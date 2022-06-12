a0bdbb56e937c76dfe39e13aef8657bc3e3127f2
Zaid <itzzaid@pm.me>
2022-06-12 12:43:35 +0530

    Update start.py

--- Zaid/plugins/start.py
+++ Zaid/plugins/start.py
@@ -3,7 +3,7 @@ from telethon import events, Button
 
 @Zaid.on(events.NewMessage(incoming=True, pattern="/start"))
 async def start(event):
-    await event.reply("Hello! Welcome To Music Bot Based On Telethon",
+    await event.reply("Hey!\nLet's Try to be a Different \nThis Bot Based on Telethon So it's Stable more.",
                     buttons=[
                         [Button.url("⚙️Support", url="https://t.me/TheSupportChat")],
                         [Button.url("🤖Repo", url="https://github.com/ITZ-ZAID/Telethon-Music")],
