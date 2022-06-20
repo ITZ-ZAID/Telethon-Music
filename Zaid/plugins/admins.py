af5e7f29bebde1dfa85e80ddc20967aa81824d36
Zaid <itzzaid@pm.me>
2022-06-21 04:05:29 +0530

    Update admins.py

--- Zaid/plugins/admins.py
+++ Zaid/plugins/admins.py
@@ -31,7 +31,7 @@ async def promote(event, perm):
     if not input_str and not user:
         await event.reply("Reply to a user or give its username to promote him!")
         return
-    sed = await Stark(GetFullUserRequest(id=user.sender_id or input_str))
+    sed = await Zaid(GetFullUserRequest(id=user.sender_id or input_str))
     await Stark(EditAdminRequest(event.chat_id, user.sender_id or input_str, ChatAdminRights(
                     add_admins=False,
                     invite_users=True,
@@ -60,7 +60,7 @@ async def promote(event, perm):
     if not input_str and not user:
         await event.reply("Reply to a user or give its username to demote him!")
         return
-    sed = await Stark(GetFullUserRequest(id=user.sender_id or input_str))
+    sed = await Zaid(GetFullUserRequest(id=user.sender_id or input_str))
     await Stark(EditAdminRequest(event.chat_id, user.sender_id or input_str, ChatAdminRights(
                     add_admins=False,
                     invite_users=None,
@@ -82,7 +82,7 @@ async def invitelink(event):
     if event.is_private:
        await event.reply("This cmd is made to be used in groups, not in PM!")
        return
-    link = await Stark(ExportChatInviteRequest(event.chat_id))
+    link = await Zaid(ExportChatInviteRequest(event.chat_id))
     await event.reply(f"Group link of {event.chat.title} is [here]({link.link})", link_preview=False)
 
 ADMIN_TEXT = """
