668135ac6d4ea112dcd9e006b76ca7ebfb8b4508
Zaid <itzzaid@pm.me>
2022-06-24 20:47:14 +0530

    Update __init__.py

--- Zaid/__init__.py
+++ Zaid/__init__.py
@@ -51,27 +51,48 @@ else:
 call_py = PyTgCalls(client)
 
 random_assistant = []
+ASSID = []
+ASSID2 = []
+ASSID3 = []
+ASSID4 = []
+ASSID5 = []
 client.start()
 call_py.start()
 random_assistant.append(1)
 random_assistant.append(6)
+if client:
+   botme = client.get_me()
+   botid = telethon.utils.get_peer_id(botme)
+   ASSID.append(botid)
 
 if client2:
    client2.start()
    call_py2.start()
    random_assistant.append(2)
+   botme2 = client2.get_me()
+   botid2 = telethon.utils.get_peer_id(botme2)
+   ASSID2.append(botid2)
 
 if client3:
    client3.start()
    call_py3.start()
    random_assistant.append(3)
+   botme3 = client3.get_me()
+   botid3 = telethon.utils.get_peer_id(botme3)
+   ASSID3.append(botid3)
 
 if client4:
    client4.start()
    call_py4.start()
    random_assistant.append(4)
+   botme4 = client4.get_me()
+   botid4 = telethon.utils.get_peer_id(botme4)
+   ASSID4.append(botid4)
 
 if client5:
    client5.start()
    call_py5.start()
    random_assistant.append(5)
+   botme5 = client5.get_me()
+   botid5 = telethon.utils.get_peer_id(botme5)
+   ASSID5.append(botid5)
