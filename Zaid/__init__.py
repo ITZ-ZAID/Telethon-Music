4fdd29745267a940e72752aa05cb7142ab0dcc33
Zaid <itzzaid@pm.me>
2022-06-24 11:13:20 +0530

    Update __init__.py

--- Zaid/__init__.py
+++ Zaid/__init__.py
@@ -21,30 +21,31 @@ client = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH)
 
 if STRING_SESSION2:
    client2 = TelegramClient(StringSession(STRING_SESSION2), API_ID, API_HASH)
+   call_py2 = PyTgCalls(client2)
 else:
    client2 = None
 
 if STRING_SESSION3:
    client3 = TelegramClient(StringSession(STRING_SESSION3), API_ID, API_HASH)
+   call_py3 = PyTgCalls(client3)
 else:
    client3 = None
 
 if STRING_SESSION4:
    client4 = TelegramClient(StringSession(STRING_SESSION4), API_ID, API_HASH)
+   call_py4 = PyTgCalls(client4)
 else:
    client4 = None
 
 if STRING_SESSION5:
    client5 = TelegramClient(StringSession(STRING_SESSION5), API_ID, API_HASH)
+   call_py5 = PyTgCalls(client5)
 else:
    client5 = None
 
 
 call_py = PyTgCalls(client)
-call_py2 = PyTgCalls(client2)
-call_py3 = PyTgCalls(client3)
-call_py4 = PyTgCalls(client4)
-call_py5 = PyTgCalls(client5)
+
 random_assistant = []
 client.start()
 call_py.start()
