7f979f7999c9665b22c2971934044260be9e9d04
Zaid <itzzaid@pm.me>
2022-06-24 22:08:07 +0530

    Update __main__.py

--- Zaid/__main__.py
+++ Zaid/__main__.py
@@ -5,7 +5,7 @@ import glob
 from pathlib import Path
 from Zaid.utils import load_plugins
 import logging
-from Zaid import Zaid
+from Zaid import * 
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.INFO)
@@ -17,6 +17,10 @@ for name in files:
         patt = Path(a.name)
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
+
+
+async def start_bot():
+    print("[INFO]: STARTING BOT CLIENT")
     
 print("Successfully Started Bot!")
 print("Visit @TheUpdatesChannel")
