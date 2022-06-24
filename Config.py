cfd6970ca588de86c0a845ee05a4f074938b7c8d
Zaid <itzzaid@pm.me>
2022-06-24 10:36:14 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,14 +1,21 @@
 import os
 from os import getenv
 
-API_ID = int(os.environ.get("APP_ID", "6435225"))
-API_HASH = os.environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-STRING_SESSION = os.environ.get("STRING_SESSION", "")
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
+
+load_dotenv()
+admins = {}
+
+API_ID = int(getenv("APP_ID", "6435225"))
+API_HASH = getenv("API_HASH", "4e984ea35f854762dcde906dce426c2d")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+STRING_SESSION = getenv("STRING_SESSION", "")
 if str(getenv("STRING_SESSION2")).strip() == "":
     STRING_SESSION2 = str(None)
 else:
     STRING_SESSION2 = str(getenv("STRING_SESSION2"))
+
 if str(getenv("STRING_SESSION3")).strip() == "":
     STRING_SESSION3 = str(None)
 else:
@@ -23,11 +30,12 @@ if str(getenv("STRING_SESSION5")).strip() == "":
     STRING_SESSION5 = str(None)
 else:
     STRING_SESSION5 = str(getenv("STRING_SESSION5"))
-MONGO_DB_URL = os.environ.get("MONGO_DB_URL", None)
-HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
-MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "Zaid2_Robot")
-SUPPORT = os.environ.get("SUPPORT", "TheSupportChat")
-CHANNEL = os.environ.get("CHANNEL", "TheUpdatesChannel")
-START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
-CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
+
+MONGO_DB_URL = getenv("MONGO_DB_URL", None)
+HEROKU_MODE = getenv("HEROKU_MODE", None)
+MANAGEMENT_MODE = getenv("MANAGEMENT_MODE", None)
+BOT_USERNAME = getenv("BOT_USERNAME", "Zaid2_Robot")
+SUPPORT = getenv("SUPPORT", "TheSupportChat")
+CHANNEL = getenv("CHANNEL", "TheUpdatesChannel")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
+CMD_IMG = getenv("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
