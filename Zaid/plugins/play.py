2f94a927658027ca733ec7021ff23e0e8ecfb187
Zaid <itzzaid@pm.me>
2022-06-24 13:19:08 +0530

    Update play.py

--- Zaid/plugins/play.py
+++ Zaid/plugins/play.py
@@ -225,6 +225,22 @@ async def play(event):
     assistant = _assistant['saveassistant']
     if event.chat_username:
        try:
+
+    else:
+       invitelink = link.link
+       try:
+           if int(assistant) == 1:
+              await cli1(ImportChatInviteRequest(invitelink))
+           if int(assistant) == 2:
+              await client2(ImportChatInviteRequest(invitelink))
+           if int(assistant) == 3:
+              await client3(ImportChatInviteRequest(invitelink))
+           if int(assistant) == 4:
+              await client4(ImportChatInviteRequest(invitelink))
+           if int(assistant) == 5:
+              await client5(ImportChatInviteRequest(invitelink))
+           if int(assistant) == 6:
+              await cli1(ImportChatInviteRequest(invitelink))
     if (
         replied
         and not replied.audio
