faf1d469e1cde52ffce9f14fd4780ef65a619a4e
Zaid <itzzaid@pm.me>
2022-06-24 10:31:30 +0530

    Update info.py

--- Zaid/plugins/info.py
+++ Zaid/plugins/info.py
@@ -1,7 +1,7 @@
 from telethon import events, Button, types
 from Zaid import Zaid
 from Zaid.status import *
-from config import Config
+import Config
 from telethon.tl.types import ChannelParticipantsAdmins
 from datetime import timedelta
 from telethon.tl.functions.photos import GetUserPhotosRequest as P
