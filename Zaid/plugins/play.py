13b65f5c42f1a3084bb18b922f832b1ce772f412
Zaid <itzzaid@pm.me>
2022-06-24 13:44:41 +0530

    Update play.py

--- Zaid/plugins/play.py
+++ Zaid/plugins/play.py
@@ -228,18 +228,18 @@ async def play(event):
         invitelink = invitelink.replace(
             "https://t.me/+", "https://t.me/joinchat/"
         )
-    if int(assistant) == 1:
-       await cli1(ImportChatInviteRequest(invitelink))
-    if int(assistant) == 2:
-       await client2(ImportChatInviteRequest(invitelink))
-    if int(assistant) == 3:
-       await client3(ImportChatInviteRequest(invitelink))
-    if int(assistant) == 4:
-       await client4(ImportChatInviteRequest(invitelink))
-    if int(assistant) == 5:
-       await client5(ImportChatInviteRequest(invitelink))
-    if int(assistant) == 6:
-       await cli1(ImportChatInviteRequest(invitelink))
+        if int(assistant) == 1:
+           await cli1(ImportChatInviteRequest(invitelink))
+        if int(assistant) == 2:
+           await client2(ImportChatInviteRequest(invitelink))
+        if int(assistant) == 3:
+           await client3(ImportChatInviteRequest(invitelink))
+        if int(assistant) == 4:
+           await client4(ImportChatInviteRequest(invitelink))
+        if int(assistant) == 5:
+           await client5(ImportChatInviteRequest(invitelink))
+        if int(assistant) == 6:
+           await cli1(ImportChatInviteRequest(invitelink))
     if (
         replied
         and not replied.audio
