08a05cd7ca6ea7ffa42ad8172b1f37d5d4fdabae
Zaid <itzzaid@pm.me>
2022-06-25 11:20:57 +0530

    Update checker.py

--- Zaid/helpers/checker.py
+++ Zaid/helpers/checker.py
@@ -54,11 +54,10 @@ def AssistantAdd(mystic):
             if event.is_group:
                 try:
                     link = await event.client(ExportChatInviteRequest(event.chat_id))
-                    invitelink = link.link
-                    if invitelink.startswith("https://t.me/+"):
-                        invitelink = invitelink.replace(
-                            "https://t.me/+", ""
-                        )
+                    invitelinkk = link.link
+                    invitelink = invitelinkk.replace(
+                        "https://t.me/+", ""
+                    )
                     if int(assistant) == 1:
                        await client(ImportChatInviteRequest(invitelink))
                     if int(assistant) == 2:
