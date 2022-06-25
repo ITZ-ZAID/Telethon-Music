70b4ec770d88a48a08239842129cd0a21e8d1c69
elieve <bayustiadi@ymail.com>
2022-06-25 22:16:08 +0800

    Update ping.py

--- Zaid/plugins/ping.py
+++ Zaid/plugins/ping.py
@@ -5,7 +5,7 @@ from telethon import Button, events
 from Zaid import *
 
 IMG = os.environ.get(
-    "PING_PIC", "https://telegra.ph/file/5edf1b910c71e385e5d57.jpg"
+    "PING_PIC", "https://telegra.ph/file/51ff101dd2e233be286a6.jpg"
 )
 ms = 4
 
@@ -16,7 +16,7 @@ ALIVE = os.environ.get(
 CAPTION = f"**꧁•⊹٭Ping٭⊹•꧂**\n\n   ⚜ {ms}\n   ⚜ ❝𝐌𝐲 𝐌𝐚𝐬𝐭𝐞𝐫❞ ~『{ALIVE}』"
 
 
-@Zaid.on(events.NewMessage(pattern="^/ping"))
+@Zaid.on(events.NewMessage(pattern="^/mping"))
 async def _(event):
-    UMM = [[Button.url("⚜ Cԋαɳɳҽʅ ⚜", "https://t.me/TheUpdatesChannel")]]
+    UMM = [[Button.url("⚜ Cԋαɳɳҽʅ ⚜", "https://t.me/w0_69")]]
     await Zaid.send_file(event.chat_id, IMG, caption=CAPTION, buttons=UMM)
