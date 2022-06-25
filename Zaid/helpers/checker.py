c6a71108bc58f3c4cc85648816a982f7c0293b24
Zaid <itzzaid@pm.me>
2022-06-25 10:35:57 +0530

    Update checker.py

--- Zaid/helpers/checker.py
+++ Zaid/helpers/checker.py
@@ -49,7 +49,7 @@ def AssistantAdd(mystic):
             await save_assistant(event.chat_id, "assistant", assis)
         assistant = _assistant['saveassistant']
         try:
-            permissions = await event.client(GetParticipantRequest(int(event.chat_id), ASSID))
+            permissions = await event.client(GetParticipantRequest(int(event.chat_id), 5154917043))
         except UserNotParticipantError:
             if event.is_group:
                 try:
