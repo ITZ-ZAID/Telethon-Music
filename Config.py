2c26c9035f0586e1ae97221b2d44c664f1acd622
elieve <bayustiadi@ymail.com>
2022-06-25 23:50:44 +0800

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("APP_ID", ""))
+    API_ID = int(os.environ.get("APP_ID", "14918875"))
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     STRING_SESSION = os.environ.get("STRING_SESSION", "")
