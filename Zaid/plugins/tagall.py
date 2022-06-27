a9486db323c779f8fb167cba5980ce68f963492c
elieve <bayustiadi@ymail.com>
2022-06-27 18:12:15 +0800

    Update tagall.py

--- Zaid/plugins/tagall.py
+++ Zaid/plugins/tagall.py
@@ -6,7 +6,7 @@ from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.tl.types import ChannelParticipantAdmin
 from telethon.tl.types import ChannelParticipantCreator
 
-from YinsRobot import telethn as client
+
 
 spam_chats = []
 
