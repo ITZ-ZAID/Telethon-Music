2ac77e8c00cedab2c3b35b001176ff044303afce
elieve <bayustiadi@ymail.com>
2022-06-27 20:37:30 +0800

    Update Config.py

--- Config.py
+++ Config.py
@@ -6,7 +6,7 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5442486445:AAGzz1Zk4sJDOiodkIbbEnnXK2G3rm_ftrg")
     STRING_SESSION = os.environ.get("STRING_SESSION", "")
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "ygabutkanbot")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "@Wo_69bot")
     SUPPORT = os.environ.get("SUPPORT", "ygabutkan")
     CHANNEL = os.environ.get("CHANNEL", "ygabutkan")
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/b167d1dce40b9011892d7.jpg")
