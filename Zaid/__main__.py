5a79c25d0ae6225178f81cfde6cf9d3834a06a97
Zaid <itzzaid@pm.me>
2022-09-10 09:15:29 +0530

    Update __main__.py

--- Zaid/__main__.py
+++ Zaid/__main__.py
@@ -1,11 +1,10 @@
 
-
-
 import glob
 from pathlib import Path
 from Zaid.utils import load_plugins
 import logging
 from Zaid import Zaid
+from Zaid import client, ASSISTANT_ID
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.INFO)
@@ -18,6 +17,14 @@ for name in files:
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
     
+async def start_bot():
+     botme = await client.get_me()
+     botid = telethon.utils.get_peer_id(botme)
+     ASSISTANT_ID.append(botid)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_bot())
+
 print("Successfully Started Bot!")
 print("Visit @TheUpdatesChannel")
 
