ae9b5d10f8efd1c05850c1fa1f002683220486d5
Zaid <itzzaid@pm.me>
2023-01-19 22:36:39 +0530

    Update __init__.py

--- Zaid/__init__.py
+++ Zaid/__init__.py
@@ -16,13 +16,11 @@ from Config import Config
 BOT_USERNAME = Config.BOT_USERNAME
 ASSISTANT_ID = Config.ASSISTANT_ID
 
-bot = TelegramClient('Zaid', api_id=Config.API_ID, api_hash=Config.API_HASH)
-Zaid = bot.start(bot_token=Config.BOT_TOKEN)
+Zaid = TelegramClient('Zaid', api_id=Config.API_ID, api_hash=Config.API_HASH, bot_token=Config.BOT_TOKEN)
+
 
 client = TelegramClient(StringSession(Config.STRING_SESSION), Config.API_ID, Config.API_HASH)
 call_py = PyTgCalls(client)
-client.start()
-call_py.start()
 
 class Bot(TelegramClient):
 
