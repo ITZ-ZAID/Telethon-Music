286db5709346c24632f1da8a1add597bc0b0d2b4
Zaid <itzzaid@pm.me>
2023-01-19 23:08:36 +0530

    Update __init__.py

--- Zaid/__init__.py
+++ Zaid/__init__.py
@@ -22,16 +22,16 @@ Zaid = TelegramClient('Zaid', api_id=Config.API_ID, api_hash=Config.API_HASH)
 client = TelegramClient(StringSession(Config.STRING_SESSION), Config.API_ID, Config.API_HASH)
 call_py = PyTgCalls(client)
 
-class Bot(TelegramClient):
+class Bot():
 
-    async def start(self):  
+    async def start():  
         await Zaid.start(bot_token=Config.BOT_TOKEN)
         await client.start()
         await call_py.start()
         print('Bot started')
 
 
-    async def stop(self, *args):
+    async def stop(*args):
         await Zaid.stop()
         await client.stop()
         await call_py.stop()
