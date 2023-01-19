bb8e012c38d6a912ff3b964029588369a546933b
Zaid <itzzaid@pm.me>
2023-01-19 22:35:11 +0530

    Update __main__.py

--- Zaid/__main__.py
+++ Zaid/__main__.py
@@ -4,7 +4,7 @@ import glob
 from pathlib import Path
 from Zaid.utils import load_plugins
 import logging
-from Zaid import Zaid
+from Zaid import Bot 
 from Zaid import client, ASSISTANT_ID
 from Zaid.plugins.autoleave import leave_from_inactive_call
 
@@ -19,20 +19,9 @@ for name in files:
         patt = Path(a.name)
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
-    
-async def start_bot():
-     print("[INFO]: LOADING ASSISTANT DETAILS")
-     botme = await client.get_me()
-     botid = telethon.utils.get_peer_id(botme)
-     print(f"[INFO]: ASSISTANT ID {botid}")
-     await asyncio.create_task(leave_from_inactive_call())
-
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(start_bot())
 
 print("[INFO]: SUCCESSFULLY STARTED BOT!")
 print("[INFO]: VISIT @TheUpdatesChannel")
 
 if __name__ == "__main__":
-    Zaid.run_until_disconnected()
+    Bot.run()
