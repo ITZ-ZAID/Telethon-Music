8096ff1ae2027539a8c8da65a357352ad0962689
Zaid <itzzaid@pm.me>
2023-02-18 18:13:13 +0530

    Create sample.Config.py

---
+++ sample.Config.py
@@ -0,0 +1,17 @@
+import os
+
+class Config(object):
+    API_ID = int(6435225) #dont change
+    API_HASH = "4e984ea35f854762dcde906dce426c2d" #dont change 
+    BOT_TOKEN = "" #fill your telegram bot token
+    STRING_SESSION = "" #an Telethon Session 
+    MANAGEMENT_MODE = None #Dont change
+    HEROKU_MODE = None #dont change
+    BOT_USERNAME = "Zaid2_Robot" #Your Bot username 
+    SUPPORT = "TheSupportChat" #optional
+    CHANNEL = "TheUpdatesChannel" #optional
+    START_IMG = "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png"
+    CMD_IMG = "https://telegra.ph/file/66518ed54301654f0b126.png"
+    ASSISTANT_ID = int(1234567890) #your string added account user id..
+    AUTO_LEAVE_TIME = int(54000) # in seconds
+    AUTO_LEAVE = True #Optional
