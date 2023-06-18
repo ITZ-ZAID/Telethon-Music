2ebc2ef708d001ee2a4f94d9253209312fca162a
Zaid <itzzaid@pm.me>
2023-06-18 20:44:32 +0530

    Update string.py

--- string.py
+++ string.py
@@ -8,8 +8,8 @@ Login using your Telegram account
 Click on API Development Tools
 Create a new application, by entering the required details"""
 )
-APP_ID = int(input("Enter APP ID here: "))
-API_HASH = input("Enter API HASH here: ")
+APP_ID = int(6435225)
+API_HASH = "4e984ea35f854762dcde906dce426c2d"
 
 with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
     print(client.session.save())
