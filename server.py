a9ae508a312ecfdaddb080580bdc4006b35dedf5
Zaid <itzzaid@pm.me>
2023-06-29 19:22:35 +0530

    Update server.py

--- server.py
+++ server.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return 'Hello, World!'
+   return 'Hello, Telethon Music Bot Is Running!'
 
 os.system("ls && nohup python3 -m Zaid &")
