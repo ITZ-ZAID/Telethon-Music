21f69e3724c97e7eede4fa7c422c98af2157d069
Zaid <itzzaid@pm.me>
2023-06-29 20:17:47 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
 import os
 from flask import Flask
+from flask_restful import Resource, Api
+
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def hello_world():
-   return 'Hello, Telethon Music Bot Is Running!'
+class Greeting (Resource):
+    def get(self):
+        return "Telethon Music is Up & Running!"
 
-os.system("ls && python3 -m Zaid")
+api.add_resource(Greeting, '/')
+app.run(host="0.0.0.0", port=os.environ.get("PORT", 8080))
