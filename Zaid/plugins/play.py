c6c7a1c5d0d38a78700a1d98c86937d4eccbdad1
Zaid <itzzaid@pm.me>
2023-10-11 15:43:50 +0530

    Update play.py

--- Zaid/plugins/play.py
+++ Zaid/plugins/play.py
@@ -196,7 +196,7 @@ async def play(event):
                     await botman.edit(f"`{ep}`")
 
     else:
-        botman = await edit_or_reply(event, "➕ Downloading File...")
+        botman = await event.reply("➕ Downloading File...")
         dl = await replied.download_media()
         link = f"https://t.me/c/{chat.id}/{event.reply_to_msg_id}"
         if replied.audio:
